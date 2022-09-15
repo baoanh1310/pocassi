a801882ca0a6abd476b393f4f473ade7f219ff3f
baoanh1310 <dataholicguy@gmail.com>
2022-09-16 01:59:58 +0700

    NFTContext for currency state sharing

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,20 @@ import { ThemeProvider } from 'next-themes';
 import Script from 'next/script';
 import '../styles/globals.css';
 
+import { NFTProvider } from '../context/NFTContext';
 import { Navbar, Footer } from '../components';
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
-    <Script src="https://kit.fontawesome.com/eb007f4b88.js" crossOrigin="anonymous" />
-  </ThemeProvider>
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+      <Script src="https://kit.fontawesome.com/eb007f4b88.js" crossOrigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default MyApp;
